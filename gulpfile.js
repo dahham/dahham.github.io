0288f6c07905c7d7f93a4c38bc57fceb072e0f79
Abdullahi Usman <abdullahismn7@gmail.com>
2018-08-28 23:33:39 +0100

    Install gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
 
 gulp.task('default', ['styles'], () => {
   gulp.watch('debug/sass/*.scss', ['styles']);
@@ -9,5 +10,6 @@ gulp.task('styles', () => {
   gulp.src('debug/sass/*.scss').pipe(sass({
       outputStyle: 'compressed'
     }).on('error', sass.logError))
+    .pipe(autoprefixer({browsers: ['last 2 vesions']}))
     .pipe(gulp.dest('css'))
 })
