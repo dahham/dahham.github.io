7982c475f67717c1edf59899e95caae2933c030c
Abdullahi Usman <abdullahismn7@gmail.com>
2018-08-28 23:56:27 +0100

    Added browser-sync to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
+const browserSync = require('browser-sync').create()
 
 gulp.task('default', ['styles', 'copy-html'], () => {
   gulp.watch('debug/sass/*.scss', ['styles']);
   gulp.watch('debug/*.html', ['copy-html'])
+
+  browserSync.init({
+    server: ''
+  })
+
 })
 
 gulp.task('styles', () => {
@@ -15,6 +21,7 @@ gulp.task('styles', () => {
       browsers: ['last 2 versions']
     }))
     .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream())
 })
 
 gulp.task('copy-html', () => {
