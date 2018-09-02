aa4ffea64777f4951c2900cdc99ea3b4bd0e5686
Abdullahi Usman <abdullahismn7@gmail.com>
2018-09-02 01:45:28 +0100

    feat: added promotional apps to be loaded dynamically

--- debug/js/main.js
+++ debug/js/main.js
@@ -43,6 +43,104 @@ function fetchAndDisplayProjects() {
   });
 }
 
+function getImageFor(image) {
+  return `/img/${image}.png`;
+}
+
+function getImageSourcesetFor(image, size) {
+  return `/img/${image}-${size}.png`;
+}
+
+function fetchAndBindPromotionalApps() {
+  fetch('/data/promotional-apps.json').then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      console.log('error while getting projects.json');
+      console.log(response);
+    }
+  }).then(json => {
+    if (json == undefined) {
+      console.log('Error while formatting json');
+    } else {
+      const apps = document.getElementById('promotional-apps');
+      for (const data of json) {
+
+        const appTitle = data['name'];
+        const appDescription = data['description'];
+        const appImage = data['image'];
+        const appLink = data['link'];
+
+        const app = document.createElement('div');
+        app.setAttribute('class', 'card');
+
+        const picture = document.createElement('picture');
+
+        const sourceLarge = document.createElement('source');
+        const sourceMedium = document.createElement('source');
+        const sourceSmall = document.createElement('source');
+
+        sourceLarge.setAttribute('media', '(min-width: 800px)');
+        sourceMedium.setAttribute('media', '(min-width: 500px)');
+        sourceSmall.setAttribute('media', '(max-width: 499px)');
+
+        sourceLarge.setAttribute('srcset', getImageSourcesetFor(appImage, 'large'));
+        sourceMedium.setAttribute('srcset', getImageSourcesetFor(appImage, 'medium'));
+        sourceSmall.setAttribute('srcset', getImageSourcesetFor(appImage, 'small'));
+
+        picture.appendChild(sourceLarge);
+        picture.appendChild(sourceMedium);
+        picture.appendChild(sourceSmall);
+
+        const img = document.createElement('img');
+        img.setAttribute('class', 'card-img-top');
+        img.setAttribute('src', getImageSourcesetFor(appImage, 'thumbnail'));
+        img.setAttribute('Alt', `${appTitle} logo`);
+
+        picture.appendChild(img);
+
+        app.appendChild(picture);
+
+        const body = document.createElement('div');
+        body.setAttribute('class', 'card-body');
+
+        const title = document.createElement('h4');
+        title.setAttribute('class', 'card-title');
+        title.innerHTML = appTitle;
+
+        body.appendChild(title);
+
+        const description = document.createElement('p');
+        description.setAttribute('class', 'card-text dahham-promotional-apps-description');
+        description.innerHTML = appDescription;
+
+        body.appendChild(description);
+
+        const link = document.createElement('a');
+        link.setAttribute('href', appLink);
+
+        const linkButton = document.createElement('button');
+        linkButton.setAttribute('class', 'btn btn-large btn-block btn-success');
+        linkButton.innerHTML = 'Download';
+
+        link.appendChild(linkButton);
+
+        body.appendChild(link);
+
+        app.appendChild(picture);
+        app.appendChild(body);
+
+        apps.appendChild(app);
+
+      }
+    }
+  });
+}
+
+function bindViews() {
+  fetchAndBindPromotionalApps();
+  fetchAndBindProjects();
+}
 if (document.readyState === 'loading') {
   self.addEventListener('DOMContentLoaded', fetchAndDisplayProjects);
 } else {
